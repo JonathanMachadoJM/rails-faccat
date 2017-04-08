300e9c7741d0713480fb72c97d41a4e4ad66833a
JonathanMachadoJM <jonathan.m@outlook.com>
2017-04-08 17:48:50 -0300

    Adicionado header dinamico para as páginas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   before_action :authenticate_admin!, except: [:show, :homepage]
   before_action :set_page, only: [ :show, :edit, :update, :destroy]
-
+ 
   # GET /pages
   def index
     @pages = Page.all
